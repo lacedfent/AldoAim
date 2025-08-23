5e859526977b15601f6d791d5cd63e0adc0f1dda
lacedfent <sillylittlepal@gmail.com>
2025-08-23 03:13:35 -0400

    added loader script to prioritize speed of the script

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local placeId = game.PlaceId
+
+if placeId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lacedfent/AldoAim/refs/heads/main/arsenal.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lacedfent/AldoAim/refs/heads/main/main.lua"))()
+end
